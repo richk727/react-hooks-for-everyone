c2d7a6a37350ab74ad8a0b87a574bf4ba75bc1dd
Richard Kaye <richk727@gmail.com>
2020-07-27 00:22:58 +0100

    Lesson #4 complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import Toggle from './Toggle';
 
 const App = () => {
   //const [value, setValue] = useState(initialState);
@@ -6,7 +7,7 @@ const App = () => {
   return (
     <div className="main-wrapper">
       <h1>Level Up Dishes</h1>
-      <h3>{name}</h3>
+      <Toggle/>
       <input type="text" onChange={(e) => setName(e.target.value)} value={name}/>
     </div>
   );
