f056bbec0946a7f811f60bc760e227c28eab8939
Richard Kaye <richk727@gmail.com>
2020-08-19 23:27:05 +0100

    Lesson #17 complete

--- src/hooks/useBodyScrollLock.js
+++ src/hooks/useBodyScrollLock.js
@@ -2,8 +2,13 @@ import { useLayoutEffect } from 'react';
 
 function useBodyScrollLock() {
     useLayoutEffect(() => {
+        const originalOverflowStyle = window.getComputedStyle(document.body).overflow;
         document.body.style.overflow = "hidden";
-    });
+
+        return () => {
+            document.body.style.overflow = originalOverflowStyle;
+        }
+    }, []);
 }
 
 export { useBodyScrollLock }
