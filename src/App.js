de889fde41a01797c9b73ab17c199c220f35cada
Richard Kaye <richk727@gmail.com>
2020-08-22 21:38:33 +0100

    Lesson #20 completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef, } from 'react';
 import useAbortableFetch from 'use-abortable-fetch';
+import { useSpring, animated } from 'react-spring';
 import Toggle from './Toggle';
 import { useTitleInput } from './hooks/useTitleInput';
 
@@ -10,18 +11,26 @@ const App = () => {
   const { data, loading, error, abort } = useAbortableFetch(
     'https://my-json-server.typicode.com/leveluptuts/fakeapi/dishes'
   );
-  if(!data) return null;
+
+  const props = useSpring({opacity: 1, from: { opacity: 0 }});
+
+
 
   return (
     <div className="main-wrapper" ref={ref}>
-      <h1 onClick={() => ref.current.classList.toggle('active')}>Level Up Dishes</h1>
+      <animated.h1 
+        style={props}
+        onClick={() => ref.current.classList.toggle('active')}>
+        Level Up Dishes
+      </animated.h1>
       <Toggle/>
       
       <form onSubmit={e => { e.preventDefault(); }}>
         <input type="text" onChange={(e) => setName(e.target.value)} value={name}/>
         <button>Submit</button>
       </form>
-      {data.map((dish, index) => (
+      {data &&
+        data.map((dish, index) => (
         <article className="dish-card dish-card--withImage" key={index}>
           <h3>{dish.title}</h3>
           <p>{dish.desc}</p>
